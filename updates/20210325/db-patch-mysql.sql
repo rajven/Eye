f98630b20e4cd4e92f84973e4de7e3cbdb6318c2
root <root@dhcpd-km135.stest.local>
2021-04-05 14:39:29 +0300

    remove unused update patches

--- updates/20210325/db-patch-mysql.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `auth_rules` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `type` INT NOT NULL , `rule` VARCHAR(40) NULL DEFAULT NULL , PRIMARY KEY (`id`), INDEX (`user_id`)) ENGINE = InnoDB;
-ALTER TABLE `auth_rules` ADD UNIQUE(`rule`);
-
--- updates/20210327/db-patch-mysql-wiki.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `User_auth` ADD `WikiName` VARCHAR(250) NULL DEFAULT NULL AFTER `dns_name`;
