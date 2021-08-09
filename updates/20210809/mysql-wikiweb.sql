a6826460408ac89660a00ee973c6e822eafdbde4
rajven <rnd@rajven.ru>
2021-08-09 17:18:09 +0300

    new options - wiki_web

---
+++ updates/20210809/mysql-wikiweb.sql
@@ -0,0 +1,2 @@
+INSERT INTO `config_options` (`id`, `option_name`, `description`, `uniq`, `type`, `default_value`, `min_value`, `max_value`) VALUES
+(63, 'wiki_web', 'Web for Wiki. Default - Main. http://example.local/Main/WebHome', 1, 'text', 'Main', 0, 0);
