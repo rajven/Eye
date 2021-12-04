ccbb787ad399902bc0435bf58699f7b726031ff1
Dmitriev Roman <rnd@sovtest.su>
2021-12-04 13:22:44 +0300

    - upload last database

---
+++ docs/mysql/create_db.sql
@@ -0,0 +1,5 @@
+--
+-- База данных: `stat`
+--
+CREATE DATABASE IF NOT EXISTS `stat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
--- docs/mysql/stat_database.sql
+++
@@ -1,5 +0,0 @@
---
--- Database: `stat`
---
-CREATE DATABASE IF NOT EXISTS `stat` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `stat`;
