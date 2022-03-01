017fb3e0b9a013e4c3d49b423bd2324a212bc090
Dmitriev Roman <rnd@sovtest.su>
2022-03-01 12:20:40 +0300

    - update mysql patch

--- updates/20220301/mysql-patch-filters.sql
+++ updates/20220301/mysql-patch-filters.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `Filter_list` ADD `srcport` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL AFTER `dstport`;
+UPDATE `Filter_list` set dstport='0' WHERE dstport IS NULL or dstport='';
+UPDATE `Filter_list` set srcport='0' WHERE srcport IS NULL or srcport='';
