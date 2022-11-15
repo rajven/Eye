ebe0f5963611de7a12d01794bc2f69286604f322
root <rnd@rajven.ru>
2022-11-15 11:38:44 +0300

    restore default id for idfilter

--- html/inc/idfilter.php
+++ html/inc/idfilter.php
@@ -3,6 +3,8 @@ if (! defined("CONFIG")) die("Not defined");
 
 if (empty($id) and !empty($_SESSION[$page_url]['id'])) { $id = $_SESSION[$page_url]['id']; }
 
+if (empty($id) and !empty($default_id)) { $id=$default_id; }
+
 if (empty($id)) {
     header("Location: /admin/index.php");
     exit;
