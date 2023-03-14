73cd49fb454f981a407d796d90ec6c5aec2f2a19
rajven <rnd@rajven.ru>
2023-03-14 19:26:06 +0300

    cosmetic changes

--- html/admin/iplist/index.php
+++ html/admin/iplist/index.php
@@ -94,9 +94,9 @@ print_ip_submenu($page_url);
 <table class="data">
 	<tr>
         <td>
-        <b><?php print WEB_list_ou; ?> - </b><?php print_ou_select($db_link, 'ou', $rou); ?>
+        <b><?php print WEB_cell_ou; ?> - </b><?php print_ou_select($db_link, 'ou', $rou); ?>
         <b><?php print WEB_rows_at_page."&nbsp"; print_row_at_pages('rows',$displayed); ?>
-        <b><?php print WEB_list_subnet; ?> - </b><?php print_subnet_select_office($db_link, 'subnet', $rsubnet); ?>
+        <b><?php print WEB_network_subnet; ?> - </b><?php print_subnet_select_office($db_link, 'subnet', $rsubnet); ?>
         <b><?php echo WEB_ips_show_by_state; ?> - </b><?php print_enabled_select('enabled', $enabled); ?>
         <b><?php echo WEB_ips_search_host; ?>:&nbsp<input type="text" name="ip" value="<?php echo $f_ip; ?>" pattern="^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])|([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}|([0-9a-fA-F]{4}[\\.-][0-9a-fA-F]{4}[\\.-][0-9a-fA-F]{4})|[0-9A-Fa-f]{12})$"/>
         <input type="submit" value="<?php echo WEB_btn_show; ?>">
