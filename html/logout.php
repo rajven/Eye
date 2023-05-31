e16871bf0b56d3bdaa3f2d02666296d0efd4358a
rajven <rnd@rajven.ru>
2023-05-31 23:05:58 +0300

    delete session after logout

--- html/logout.php
+++ html/logout.php
@@ -1,4 +1,7 @@
 <?php
 require_once ($_SERVER['DOCUMENT_ROOT']."/inc/auth.utils.php");
+if (isset($_SESSION['session_id'])) {
+    run_sql($db_link, "DELETE FROM sessions WHERE session_id='".$_SESSION['session_id']."'");
+    }
 logout();
 ?>
