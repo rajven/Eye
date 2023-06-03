d8378c6bbe736c25aa00f1df37cd288552701a17
rajven <rnd@rajven.ru>
2023-06-03 23:02:00 +0300

    fix action for dns filters

--- updates/2-4-11/filter-uprades.sql
+++ updates/2-4-11/filter-uprades.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `Group_filters` ADD `action` BOOLEAN NOT NULL DEFAULT FALSE AFTER `order`;
 UPDATE `Group_filters` as G set action = (SELECT action FROM Filter_list WHERE G.filter_id = id);
 ALTER TABLE `Filter_list` DROP `action`;
+DELETE FROM Group_filters WHERE filter_id NOT IN (SELECT id FROM Filter_list);
+DELETE FROM Group_filters WHERE group_id NOT IN (SELECT id FROM Group_list);
