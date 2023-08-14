8970bfba1687ecc2606668ac4121d16ba8112c06
Roman Dmitriev <rnd@rajven.ru>
2023-08-14 13:57:41 +0300

    fix edit device page - remove snmp fdb element title

--- html/admin/devices/editdevice.php
+++ html/admin/devices/editdevice.php
@@ -131,8 +131,8 @@ if ($device['device_type']==2) {
     print "<tr><td colspan=4 class='data'>"; print get_l3_interfaces($db_link,$device['id']); print "</td></tr>";
     }
 if ($device['device_type']<=2) {
-    print "<tr><td>".WEB_snmp_version."</td><td><p title='".WEB_device_snmp_hint."'>".WEB_device_mac_by_oid."</p></td>";
-    print "<td>".WEB_network_discovery."</td><td>".WEB_nagios."</td></tr>";
+    print "<tr><td>".WEB_snmp_version."</td>";
+    print "<td>".WEB_network_discovery."</td><td>".WEB_nagios."</td><td></td></tr>";
     print "<tr><td class='data'>"; print_snmp_select('f_snmp_version', $device['snmp_version']); print "</td>\n";
     print "<td class='data'>"; print_qa_select('f_discovery', $device['discovery']); print "</td>\n";
     print "<td class='data'>"; print_qa_select('f_nagios', $device['nagios']); print "</td>\n";
