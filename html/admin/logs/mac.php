425a8d383ba98264d64b95e7be455244c4554091
Roman Dmitriev <rnd@rajven.ru>
2023-09-20 11:20:54 +0300

    stupid mistake in show mac log

--- html/admin/logs/mac.php
+++ html/admin/logs/mac.php
@@ -28,7 +28,7 @@ print_log_submenu($page_url);
 </form>
 
 <?php
-$countSQL="SELECT Count(*) FROM mac_history WHERE `timestamp`>='$date1' AND `timestamp`<'$date2' ORDER BY id DESC $mac_where";
+$countSQL="SELECT Count(*) FROM mac_history WHERE `timestamp`>='$date1' AND `timestamp`<'$date2' $mac_where ORDER BY id DESC";
 $res = mysqli_query($db_link, $countSQL);
 $count_records = mysqli_fetch_array($res);
 
