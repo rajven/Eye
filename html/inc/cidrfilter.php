02432d65ac5ccd278efb14c2048a715fef0fd7b5
Roman Dmitriev <rnd@rajven.ru>
2023-09-27 18:41:47 +0300

    second stuped mistake

--- html/inc/cidrfilter.php
+++ html/inc/cidrfilter.php
@@ -2,8 +2,8 @@
 if (! defined("CONFIG")) die("Not defined");
 
 if (!isset($default_cidr)) { $default_cidr = ''; }
-if (isset($_GET['cidr'])) { $rcidr = $_GET["cidr"] * 1; }
-if (isset($_POST['cidr'])) { $rcidr = $_POST["cidr"] * 1; }
+if (isset($_GET['cidr'])) { $rcidr = $_GET["cidr"]; }
+if (isset($_POST['cidr'])) { $rcidr = $_POST["cidr"]; }
 if (! isset($rcidr)) {
     if (isset($_SESSION[$page_url]['cidr'])) { $rcidr=$_SESSION[$page_url]['cidr']; } else { $rcidr = $default_cidr; }
     }
