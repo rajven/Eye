6f4dd03e20847f3e1297a74f0d28c23e97c517fb
Roman Dmitriev <rnd@rajven.ru>
2023-10-06 15:16:04 +0300

    fix log option82

--- docs/Readme.ru.md
+++ docs/Readme.ru.md
@@ -3,7 +3,7 @@
 1. Ставим пакеты
 
 apt install apache2 git fping perl mariadb-server php php-mysql php-bcmath php-intl \
-php-mbstring php-date php-mail php-snmp php-zip \
+php-mbstring php-date php-mail php-snmp php-zip xxd \
 libnet-patricia-perl libnetaddr-ip-perl libconfig-tiny-perl libnet-dns-perl libdatetime-perl \
 libnet-netmask-perl libtext-iconv-perl libnet-snmp-perl libnet-telnet-perl libdbi-perl \
 libdbd-mysql-perl libparallel-forkmanager-perl libproc-daemon-perl libdatetime-format-dateparse-perl \
--- scripts/dnsmasq-hook.sh
+++ scripts/dnsmasq-hook.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 time=$(date +%s)
-echo "$1;$2;$3;$4;${time};${DNSMASQ_TAGS};${DNSMASQ_SUPPLIED_HOSTNAME};${DNSMASQ_OLD_HOSTNAME};${DNSMASQ_CIRCUIT_ID};${DNSMASQ_REMOTE_ID};${DNSMASQ_CLIENT_ID}" >>/var/log/dhcp.log &
+
+CIRCUIT_ID=$(echo ${DNSMASQ_CIRCUIT_ID}  | /usr/bin/xxd -l 16 -p )
+REMOTE_ID=$(echo ${DNSMASQ_REMOTE_ID} | /usr/bin/xxd -l 16 -p )
+
+echo "$1;$2;$3;$4;${time};${DNSMASQ_TAGS};${DNSMASQ_SUPPLIED_HOSTNAME};${DNSMASQ_OLD_HOSTNAME};${CIRCUIT_ID};${REMOTE_ID};${DNSMASQ_CLIENT_ID}" >>/var/log/dhcp.log &
 
 exit
