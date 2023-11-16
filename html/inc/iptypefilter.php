edf16b266fcbbdc79865fbb096e4fcb7f2817f17
Roman Dmitriev <rnd@rajven.ru>
2023-11-16 12:38:59 +0300

    fix mistake in ip filter field

--- html/inc/iptypefilter.php
+++ html/inc/iptypefilter.php
@@ -1,8 +1,8 @@
 <?php
 if (! defined("CONFIG")) die("Not defined");
 
-if (isset($_GET['ip-type'])) { $ip_type = $_GET["ip_type"] * 1; }
-if (isset($_POST['ip-type'])) { $ip_type = $_POST["ip_type"] * 1; }
+if (isset($_GET['ip_type'])) { $ip_type = $_GET["ip_type"] * 1; }
+if (isset($_POST['ip_type'])) { $ip_type = $_POST["ip_type"] * 1; }
 if (!isset($ip_type)) {
     if (isset($_SESSION[$page_url]['ip_type'])) { $ip_type = $_SESSION[$page_url]['ip_type']*1; }
     }
