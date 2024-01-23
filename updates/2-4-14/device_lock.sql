3dc103126097b9be6caf71d912bd0cad4301e9fd
Roman Dmitriev <rnd@sovtest.su>
2024-01-23 19:00:35 +0300

    fix mysql schema

--- updates/2-4-14/device_lock.sql
+++ updates/2-4-14/device_lock.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `devices` ADD `discovery_locked` BOOLEAN NOT NULL DEFAULT FALSE AFTER `deleted`, ADD `locked_timestamp` DATETIME NULL DEFAULT NULL AFTER `discovery_locked`;
+ALTER TABLE `devices` ADD `discovery_locked` BOOLEAN NOT NULL DEFAULT FALSE AFTER `deleted`, ADD `locked_timestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP AFTER `discovery_locked`;
