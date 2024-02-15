19a46c33cb2eaf594a2f340e3fdbccbbdab54b1d
Roman Dmitriev <rnd@sovtest.su>
2024-02-15 17:21:14 +0300

    latest release 2.5.2

--- scripts/updates/upgrade.pl
+++ scripts/updates/upgrade.pl
@@ -30,12 +30,13 @@ my @old_releases = (
 '2.4.12',
 '2.4.14',
 '2.5.1',
+'2.5.2',
 );
 
 my $r_index = 0;
 my %old_releases_h = map {$_ => $r_index++ } @old_releases;
 
-my $eye_release = '2.5.1';
+my $eye_release = '2.5.2';
 
 if (!$config_ref{version}) { $config_ref{version}='2.4.12'; }
 
