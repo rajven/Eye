fb4ebe0677af1180ed1a58ee6d687fda3fddd369
Roman Dmitriev <rnd@sovtest.su>
2024-06-08 20:59:03 +0300

    fixed display error log events

--- html/inc/loglevelfilter.php
+++ html/inc/loglevelfilter.php
@@ -1,14 +1,23 @@
 <?php
-if (! defined("CONFIG")) die("Not defined");
+if (!defined("CONFIG")) die("Not defined");
 
 if (isset($_POST['display_log_level']) or isset($_GET['display_log_level'])) {
-    if (isset($_GET['display_log_level'])) { $display_log_level = $_GET['display_log_level']*1; }
-    if (isset($_POST['display_log_level'])) { $display_log_level = $_POST['display_log_level']*1; }
-    } else {
-    if (isset($_SESSION[$page_url]['display_log_level'])) { $display_log_level=$_SESSION[$page_url]['display_log_level']; } else { $display_log_level = 1; }
+    if (isset($_GET['display_log_level'])) {
+        $display_log_level = $_GET['display_log_level'] * 1;
     }
+    if (isset($_POST['display_log_level'])) {
+        $display_log_level = $_POST['display_log_level'] * 1;
+    }
+} else {
+    if (isset($_SESSION[$page_url]['display_log_level'])) {
+        $display_log_level = $_SESSION[$page_url]['display_log_level'];
+    } else {
+        $display_log_level = 1;
+    }
+}
 
-if (empty($display_log_level)) { $display_log_level=1; }
+if (!isset($display_log_level)) {
+    $display_log_level = 1;
+}
 
-$_SESSION[$page_url]['display_log_level']=$display_log_level;
-?>
+$_SESSION[$page_url]['display_log_level'] = $display_log_level;
