3d1b9a2328cf24b895bd884992050a5cb97308b1
root <rnd@rajven.ru>
2025-04-10 16:09:14 +0300

    outdated code has been removed

--- docs/mysql/reset_admin.sql
+++ docs/mysql/reset_admin.sql
@@ -1,3 +1,3 @@
 --- default user: admin
 --- default password admin
-INSERT INTO `Customers` (`id`, `Login`, `password`, `api_key`, `readonly`) VALUES (1, 'admin', '$2y$11$wohV8Tuqu0Yai9Shacei5OKfMxG5bnLxB5ACcZcJJ3pYEbIH0qLGG', 'c3284d0f94606de1fd2af172aba15bf31', 0) ON DUPLICATE KEY UPDATE Login="admin", password="$2y$11$wohV8Tuqu0Yai9Shacei5OKfMxG5bnLxB5ACcZcJJ3pYEbIH0qLGG";
+INSERT INTO `Customers` (`id`, `Login`, `password`, `api_key`,`rights`) VALUES (1, 'admin', '$2y$11$wohV8Tuqu0Yai9Shacei5OKfMxG5bnLxB5ACcZcJJ3pYEbIH0qLGG', 'c3284d0f94606de1fd2af172aba15bf31','1') ON DUPLICATE KEY UPDATE Login="admin", password="$2y$11$wohV8Tuqu0Yai9Shacei5OKfMxG5bnLxB5ACcZcJJ3pYEbIH0qLGG";
--- html/inc/qauth.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once ("auth.utils.php");
-Silentlogin($db_link);
-?>
