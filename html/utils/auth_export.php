d718688e8aec8ddb78e856618ef286213f63be19
root <rnd@rajven.ru>
2026-01-13 18:01:45 +0300

    fix mistake

--- html/utils/auth_export.php
+++ html/utils/auth_export.php
@@ -1,6 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/inc/auth.php");
-require_once($_SERVER['DOCUMENT_OUT'] . "/inc/languages/" . HTML_LANG . ".php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/inc/languages/" . HTML_LANG . ".php");
 
 if (!defined("CONFIG")) die("Not defined");
 
